03e495c631b7c57ca665c90ab73e05feb1343283
Jannik Noe <jannik-ersterseinesnamenskonigdertuntenunddererstenmenschen@MacBook-Pro.fritz.box>
2022-09-08 14:50:51 +0200

    final website

---
+++ ressourcen/js/main.js
@@ -0,0 +1,12 @@
+function menu (){
+    let navmenu = document.getElementById('navmenu');
+
+    if (navmenu.style.display =='none'){
+        navmenu.style.display = 'block';
+    }else{
+        navmenu.style.display = 'none';
+    }
+    console.log(navmenu);
+
+
+}
